cdbc4cddf6cb9a02e66e06d7bd7306c2fbc312db
By-Alvarez06 <bvalvarez1.1@gmail.com>
2026-06-11 12:14:26 -0500

    update: Cambios en __str__ de ejemplo07/ejemplo01 Academia

--- ejemplo07/ejemplo01/academia/models.py
+++ ejemplo07/ejemplo01/academia/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Estudiante(models.Model):
@@ -9,4 +10,18 @@ class Estudiante(models.Model):
 
 
     def __str__(self):
-        return f"{self.nombre} - {self.apellido} - {self.cedula} - edad: {self.edad}"
+        return (f"Nombre: {self.nombre} - Apellido: {self.apellido} "
+                f"- Edad: {self.edad} - Año de nacimiento: {self.obtener_anio_nacimiento()} "
+                f"- Ciudad: {self.obtener_ciudad()}")
+    
+    def obtener_anio_nacimiento(self):
+        # Obtener el año actual con Datetime
+        anio_actual = datetime.date.today().year
+        return anio_actual - self.edad
+    
+    def obtener_ciudad(self):
+        # Si la cedula empieza con 11, es de Loja, sino es de 'Otra ciudad'
+        if self.cedula.startswith('11'):
+            return 'Loja'
+        else:
+            return 'Otra ciudad'
